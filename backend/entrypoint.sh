66d3d561715c5b901feced9e51910f7df70eb53c
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-20 14:38:28 +0300

    Simplify backend docker file

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 set -e
 
-echo "----- 1. Waiting for Postgres... -----"
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-    sleep 0.1
+postgres_ready() {
+python << END
+import sys
+import psycopg2
+try:
+    psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="${POSTGRES_HOST}",
+        port="${POSTGRES_PORT}",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+until postgres_ready; do
+  >&2 echo '-----> Waiting for PostgreSQL to become available...'
+  sleep 1
 done
-echo "Postgres started."
+>&2 echo '-----> PostgreSQL is available'
 
 echo "----- 2. Collecting static files... -----"
 python manage.py collectstatic --noinput
